52e59252d900292d18d770f5ec04a5984eeef026
Dishan(Shayon!) <163113315+DISHAN-cyber@users.noreply.github.com>
2026-07-01 12:53:23 +0530

    create New pw screen

---
+++ lib/screens/new_password_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class NewPasswordScreen extends StatelessWidget {
+  const NewPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFDF5F9),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'New Password\nScreen Coming Next',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, color: Color(0xFF6B4C7A)),
+        ),
+      ),
+    );
+  }
+}
